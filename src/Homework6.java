d587f10b05a2392f84758332f36293953e8089c3
Kasey Delk <delkk@student.swosu.edu>
2015-10-07 18:43:49 -0500

    Added the outline for our problem to the dev branch

--- src/Homework6.java
+++ src/Homework6.java
@@ -1,5 +1,30 @@
 public class Homework6 {
 	public static void main(String[] args) {
 
+		/*Round 1
+		* Generate 2 random numbers
+		* Ask the user to add these two numbers together
+		* Read their response 
+		* Check if the answer was correct
+		* If correct
+		* Tell them it was correct
+	 	* & give them points
+		* Make the next question harder
+		* If not correct
+		* Tell them the correct answer
+		* & do not give them points
+		* Make next question easier
+		* 
+		* Round 2
+		* Tell them it was correct
+		* Give them points
+		* Make the next question harder
+		*  
+		* Round 3
+		* Tell them it was correct
+		* Give them points
+		* Make the next question harder	 	
+		*/
+
 	}
 }
