85d3d4817ac0fe345cc682755c7722dcb860fccb
Kasey Delk <delkk@student.swosu.edu>
2015-10-09 09:27:05 -0500

    copied round 1 outline to other rounds

--- src/Homework6.java
+++ src/Homework6.java
@@ -16,19 +16,46 @@ public class Homework6 {
 		* 	Make next question easier
 		* 
 		* Round 2
-		* Tell them it was correct
-		* Give them points
+		* Generate 2 random numbers
+		* Ask the user to add these two numbers together
+		* Read their response 
+		* Check if the answer was correct
+		* 	If correct
+		* 	Tell them it was correct
+	 	* 	& give them points
 		* Make the next question harder
+		* 	If not correct
+		* 	Tell them the correct answer
+		* 	& do not give them points
+		* 	Make next question easi
 		*  
 		* Round 3
-		* Tell them it was correct
-		* Give them points
+		* Generate 2 random numbers
+		* Ask the user to add these two numbers together
+		* Read their response 
+		* Check if the answer was correct
+		* 	If correct
+		* 	Tell them it was correct
+	 	* 	& give them points
 		* Make the next question harder
+		* 	If not correct
+		* 	Tell them the correct answer
+		* 	& do not give them points
+		* 	Make next question easi
 		* 
 		* Round 4	
-		* Tell them it was correct
-		* Give them points
-		* Make the next question harder 	
+		*  Generate 2 random numbers
+		* Ask the user to add these two numbers together
+		* Read their response 
+		* Check if the answer was correct
+		* 	If correct
+		* 	Tell them it was correct
+	 	* 	& give them points
+		* Make the next question harder
+		* 	If not correct
+		* 	Tell them the correct answer
+		* 	& do not give them points
+		* 	Make next question easi	
 		*/
 
 	}
