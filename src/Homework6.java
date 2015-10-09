ce45917fcedda41d54260e331dff21e94fa2a88e
Kasey Delk <delkk@student.swosu.edu>
2015-10-09 09:22:38 -0500

    indented if statements for easier reading

--- src/Homework6.java
+++ src/Homework6.java
@@ -6,14 +6,14 @@ public class Homework6 {
 		* Ask the user to add these two numbers together
 		* Read their response 
 		* Check if the answer was correct
-		* If correct
-		* Tell them it was correct
-	 	* & give them points
+		* 	If correct
+		* 	Tell them it was correct
+	 	* 	& give them points
 		* Make the next question harder
-		* If not correct
-		* Tell them the correct answer
-		* & do not give them points
-		* Make next question easier
+		* 	If not correct
+		* 	Tell them the correct answer
+		* 	& do not give them points
+		* 	Make next question easier
 		* 
 		* Round 2
 		* Tell them it was correct
