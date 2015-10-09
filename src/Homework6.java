c03541dd2bf0687ee32a97ec429a3978520442e7
Kasey Delk <delkk@student.swosu.edu>
2015-10-09 09:53:46 -0500

    wrote code under Dr. Evert's instruction

--- src/Homework6.java
+++ src/Homework6.java
@@ -1,14 +1,34 @@
+import java.util.Scanner;
 public class Homework6 {
 	public static void main(String[] args) {
 
-		   /*This is my development branch
-			* 
-			* Round 1
-			* Generate 2 random numbers
-			* Ask the user to add these two numbers together
-			* Read their response 
-			* Check if the answer was correct
-			* 	If correct
+		   	//This is my development branch
+			//Round 1
+		
+			//Generate 2 random numbers
+			int number1 = (int) (Math.random () * 10);
+			System.out.println("Number is " + number1);
+			
+			int number2 = (int) (Math.random () * 10);
+			System.out.println("Number is " + number2);
+			int correctAnswer = number1 + number2;
+			//Ask the user to add these two numbers together
+			System.out.println("What is " + number1 + " + " + number2 + "?");
+			 
+			//Read their response 
+			Scanner input = new Scanner(System.in);
+			int userAnswer = input.nextInt();
+			
+		 
+		    //Check if the answer was correct
+			//If correct
+			if (userAnswer == correctAnswer){
+				System.out.println("Answer was correct.");
+			}else{
+				System.out.println("Answer is incorrect.");
+			}
+		
+			/* 
 			* 	Tell them it was correct
 		 	* 	& give them points
 			* Make the next question harder
