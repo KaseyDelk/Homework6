6867e2bb1ca129568b56358571fb3c5cb457706d
Kasey Delk <delkk@student.swosu.edu>
2015-10-09 09:38:46 -0500

    noted development branch in my outline

--- src/Homework6.java
+++ src/Homework6.java
@@ -1,62 +1,64 @@
 public class Homework6 {
 	public static void main(String[] args) {
 
-		/*Round 1
-		* Generate 2 random numbers
-		* Ask the user to add these two numbers together
-		* Read their response 
-		* Check if the answer was correct
-		* 	If correct
-		* 	Tell them it was correct
-	 	* 	& give them points
-		* Make the next question harder
-		* 	If not correct
-		* 	Tell them the correct answer
-		* 	& do not give them points
-		* 	Make next question easier
-		* 
-		* Round 2
-		* Generate 2 random numbers
-		* Ask the user to add these two numbers together
-		* Read their response 
-		* Check if the answer was correct
-		* 	If correct
-		* 	Tell them it was correct
-	 	* 	& give them points
-		* Make the next question harder
-		* 	If not correct
-		* 	Tell them the correct answer
-		* 	& do not give them points
-		* 	Make next question easier
-		*  
-		* Round 3
-		* Generate 2 random numbers
-		* Ask the user to add these two numbers together
-		* Read their response 
-		* Check if the answer was correct
-		* 	If correct
-		* 	Tell them it was correct
-	 	* 	& give them points
-		* Make the next question harder
-		* 	If not correct
-		* 	Tell them the correct answer
-		* 	& do not give them points
-		* 	Make next question easier
-		* 
-		* Round 4	
-		*  Generate 2 random numbers
-		* Ask the user to add these two numbers together
-		* Read their response 
-		* Check if the answer was correct
-		* 	If correct
-		* 	Tell them it was correct
-	 	* 	& give them points
-		* Make the next question harder
-		* 	If not correct
-		* 	Tell them the correct answer
-		* 	& do not give them points
-		* 	Make next question easier	
-		*/
+		   /*This is my development branch
+			* 
+			* Round 1
+			* Generate 2 random numbers
+			* Ask the user to add these two numbers together
+			* Read their response 
+			* Check if the answer was correct
+			* 	If correct
+			* 	Tell them it was correct
+		 	* 	& give them points
+			* Make the next question harder
+			* 	If not correct
+			* 	Tell them the correct answer
+			* 	& do not give them points
+			* 	Make next question easier
+			* 
+			* Round 2
+			* Generate 2 random numbers
+			* Ask the user to add these two numbers together
+			* Read their response 
+			* Check if the answer was correct
+			* 	If correct
+			* 	Tell them it was correct
+		 	* 	& give them points
+			* Make the next question harder
+			* 	If not correct
+			* 	Tell them the correct answer
+			* 	& do not give them points
+			* 	Make next question easier
+			*  
+			* Round 3
+			* Generate 2 random numbers
+			* Ask the user to add these two numbers together
+			* Read their response 
+			* Check if the answer was correct
+			* 	If correct
+			* 	Tell them it was correct
+		 	* 	& give them points
+			* Make the next question harder
+			* 	If not correct
+			* 	Tell them the correct answer
+			* 	& do not give them points
+			* 	Make next question easier
+			* 
+			* Round 4	
+			*  Generate 2 random numbers
+			* Ask the user to add these two numbers together
+			* Read their response 
+			* Check if the answer was correct
+			* 	If correct
+			* 	Tell them it was correct
+		 	* 	& give them points
+			* Make the next question harder
+			* 	If not correct
+			* 	Tell them the correct answer
+			* 	& do not give them points
+			* 	Make next question easier	
+			*/
 
 	}
 }
