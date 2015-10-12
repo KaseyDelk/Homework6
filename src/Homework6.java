b6cadd83f20f73a501fc68e62dcebc8ffca013be
Kasey Delk <delkk@student.swosu.edu>
2015-10-12 16:09:13 -0500

    cleaned up Dr. Evert's outline

--- src/Homework6.java
+++ src/Homework6.java
@@ -3,50 +3,47 @@ public class Homework6 {
 	public static void main(String[] args) {
 
 				System.out.println("Hello Class!");
-				// This is my master branch.
-				// Outline for addition problem.
-				
+				// This is my branch I use for classwork
+		
 				int score = 0;
 				int hardnessOfQuestion = 10;
 				
-				// Round 1
-				//	Generate 2 random numbers
-				int number1 = (int)(Math.random() * hardnessOfQuestion);
-				System.out.println("Number1 is :" + number1 );
-				
+				//Round 1
+				//Generate 2 random numbers
+				int number1 = (int)(Math.random() * hardnessOfQuestion);	
 				int number2 = (int)(Math.random() * hardnessOfQuestion);
-				System.out.println("Number2 is :" + number2 );
 				
-				// to keep track of the correct answer.
+				//Note the correct answer.
 				int correctAnswer = number1 + number2;
 				
-				//  Ask the user to add these two numbers together
+				//Ask the user to add these two numbers together
 				System.out.println
 				("What is " + number1 + " + " + number2 + "?");
 				
-				//  Read in their response
+				//Read their response
 				Scanner input = new Scanner(System.in);
 				int studentAnswer = input.nextInt();
-				//  Check if the answer was correct
-				//		IF correct
+				
+				//Check if the answer was correct
+				//IF correct
 				if (studentAnswer == correctAnswer){
-					//			Tell them it was correct
-					System.out.println("Answer was correct");
-					//			Give them points
-					score += hardnessOfQuestion;
-					System.out.println("Score is: " + score);
-					//			Make the next question harder
-					hardnessOfQuestion *= 10;	
-					System.out.println("hardness is: " + hardnessOfQuestion);
+				//Tell them it was correct
+				System.out.println("Answer was correct");
+				//Give them points
+				score += hardnessOfQuestion;
+				System.out.println("Score is: " + score);
+				//Make the next question harder
+				hardnessOfQuestion *= 10;	
+				System.out.println("hardness is: " + hardnessOfQuestion);
 				}
-				//		IF not correct
+				//IF not correct
 				else{
-					//			Tell them it was wrong
-					System.out.println("Answer was not correct");
-					// 			Tell them the correct answer
-					System.out.println("The correct answer was: " + correctAnswer);
-					//			Do not give them points
-					//			Make the next question easier
+				//Tell them it was wrong
+				System.out.println("Answer was not correct");
+				//Tell them the correct answer
+				System.out.println("The correct answer was: " + correctAnswer);
+				//Do not give them points
+				//Make the next question easier
 		}
 	}
 }
