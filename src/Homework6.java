0a8264b2a4466b8ad64c57a0714b35b3caa0a9bf
Kasey Delk <delkk@student.swosu.edu>
2015-10-12 10:44:32 -0500

    changed code to reflect classwork

--- src/Homework6.java
+++ src/Homework6.java
@@ -2,108 +2,51 @@ import java.util.Scanner;
 public class Homework6 {
 	public static void main(String[] args) {
 
-		   	//This is the development branch
-			
-			int score = 0;
-			int hardnessofquestion = 10;
-			
-			//Round 1
-			//Generate 2 random numbers
-			int number1round1 = (int) (Math.random () * hardnessofquestion);
-			int number2round1 = (int) (Math.random () * hardnessofquestion);
-			int correctAnswer1 = number1round1 + number2round1;
-			
-			//Ask the user to add these two numbers together
-			System.out.println("What is " + number1round1 + " + " + number2round1 + "?");
-			 
-			//Read their response 
-			Scanner input = new Scanner(System.in);
-			int userAnswer1 = input.nextInt();
-
-		    //Check if the answer was correct
-			if (userAnswer1 == correctAnswer1){
-				System.out.println("Answer was correct.");
-				hardnessofquestion *= 10;
-				System.out.println("Score is: " + hardnessofquestion);
-			}else{
-				System.out.println("Answer is incorrect.");
-				System.out.println("You recieved no points.");
-				System.out.println("Answer was " + correctAnswer1);
-			}
-			
-			//Round 2
-			int hardnessofquestion2 = 100;
-			
-			//Generate 2 random numbers
-			int number1round2 = (int) (Math.random () * hardnessofquestion);
-			int number2round2 = (int) (Math.random () * hardnessofquestion);
-			int correctAnswer2 = number1round2 + number2round2;
-			
-			//Ask the user to add these two numbers together
-			System.out.println("What is " + number1round2 + " + " + number2round2 + "?");
-			 
-			//Read their response 
-			int userAnswer2 = input.nextInt();
-
-		    //Check if the answer was correct
-			if (userAnswer2 == correctAnswer2){
-				System.out.println("Answer was correct.");
-				hardnessofquestion *= 10;
-				System.out.println("Score is: " + hardnessofquestion);
-			}else{
-				System.out.println("Answer is incorrect.");
-				System.out.println("You recieved no points.");
-				System.out.println("Answer was " + correctAnswer2);
-			}
-			
-			//Round 3
-			int hardnessofquestion3 = 1000;
-			
-			//Round 1
-			//Generate 2 random numbers
-			int number1round3 = (int) (Math.random () * hardnessofquestion);
-			int number2round3 = (int) (Math.random () * hardnessofquestion);
-			int correctAnswer3 = number1round3 + number2round3;
-			
-			//Ask the user to add these two numbers together
-			System.out.println("What is " + number1round3 + " + " + number2round3 + "?");
-			 
-			//Read their response 
-			int userAnswer3 = input.nextInt();
-
-		    //Check if the answer was correct and award points if necessary 
-			if (userAnswer3 == correctAnswer3){
-				System.out.println("Answer was correct.");
-				hardnessofquestion *= 10;
-				System.out.println("Score is: " + hardnessofquestion);
-			}else{
-				System.out.println("Answer is incorrect.");
-				System.out.println("You recieved no points.");
-				System.out.println("Answer was " + correctAnswer3);
-			}
-			//Round 4
-			int hardnessofquestion4 = 1000;
-			
-			//Generate 2 random numbers
-			int number1round4 = (int) (Math.random () * hardnessofquestion);
-			int number2round4 = (int) (Math.random () * hardnessofquestion);
-			int correctAnswer4 = number1round4 + number2round4;
-			
-			//Ask the user to add these two numbers together
-			System.out.println("What is " + number1round4 + " + " + number2round4 + "?");
-			 
-			//Read their response 
-			int userAnswer4 = input.nextInt();
-
-		    //Check if the answer was correct
-			if (userAnswer4 == correctAnswer4){
-				System.out.println("Answer was correct.");
-				hardnessofquestion *= 10;
-				System.out.println("Score is: " + hardnessofquestion);
-			}else{
-				System.out.println("Answer is incorrect.");
-				System.out.println("You recieved no points.");
-				System.out.println("Answer was " + correctAnswer4);
-			}
+				System.out.println("Hello Class!");
+				// This is my master branch.
+				// Outline for addition problem.
+				
+				int score = 0;
+				int hardnessOfQuestion = 10;
+				
+				// Round 1
+				//	Generate 2 random numbers
+				int number1 = (int)(Math.random() * hardnessOfQuestion);
+				System.out.println("Number1 is :" + number1 );
+				
+				int number2 = (int)(Math.random() * hardnessOfQuestion);
+				System.out.println("Number2 is :" + number2 );
+				
+				// to keep track of the correct answer.
+				int correctAnswer = number1 + number2;
+				
+				//  Ask the user to add these two numbers together
+				System.out.println
+				("What is " + number1 + " + " + number2 + "?");
+				
+				//  Read in their response
+				Scanner input = new Scanner(System.in);
+				int studentAnswer = input.nextInt();
+				//  Check if the answer was correct
+				//		IF correct
+				if (studentAnswer == correctAnswer){
+					//			Tell them it was correct
+					System.out.println("Answer was correct");
+					//			Give them points
+					score += hardnessOfQuestion;
+					System.out.println("Score is: " + score);
+					//			Make the next question harder
+					hardnessOfQuestion *= 10;	
+					System.out.println("hardness is: " + hardnessOfQuestion);
+				}
+				//		IF not correct
+				else{
+					//			Tell them it was wrong
+					System.out.println("Answer was not correct");
+					// 			Tell them the correct answer
+					System.out.println("The correct answer was: " + correctAnswer);
+					//			Do not give them points
+					//			Make the next question easier
+		}
 	}
 }
