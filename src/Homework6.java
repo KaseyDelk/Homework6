642d225d0d9de49a2c2d4b005dd1f2bef0449ca5
Kasey Delk <delkk@student.swosu.edu>
2015-10-12 09:34:19 -0500

    added round 3 code

--- src/Homework6.java
+++ src/Homework6.java
@@ -68,22 +68,41 @@ public class Homework6 {
 			* 	& do not give them points
 			* 	Make next question easier
 			*/ 
-			/* Round 3
-			* Generate 2 random numbers
-			* Ask the user to add these two numbers together
-			* Read their response 
-			* Check if the answer was correct
-			* 	If correct
-			* 	Tell them it was correct
+			
+			//Round 3
+			//Generate 2 random numbers
+			int number1round3 = (int) (Math.random () * 10);
+			System.out.println("Number is " + number1round1);
+			
+			int number2round3 = (int) (Math.random () * 10);
+			System.out.println("Number is " + number2round3);
+			int correctAnswer3 = number1round3 + number2round3;
+			
+			//Ask the user to add these two numbers together
+			System.out.println("What is " + number1round3 + " + " + number2round3 + "?");
+			 
+			//Read their response 
+			int userAnswer3 = input.nextInt();
+
+		    //Check if the answer was correct
+			//give them points
+			if (userAnswer == correctAnswer){
+				System.out.println("Answer was correct. You received 10 points.");
+			}else{
+				System.out.println("Answer is incorrect. You received 0 points. ");
+			}
+			
+			/* 	Tell them it was correct
 		 	* 	& give them points
 			* Make the next question harder
 			* 	If not correct
 			* 	Tell them the correct answer
 			* 	& do not give them points
 			* 	Make next question easier
-			* 
-			* Round 4	
-			*  Generate 2 random numbers
+			*/
+			
+			// Round 4	
+			/*  Generate 2 random numbers
 			* Ask the user to add these two numbers together
 			* Read their response 
 			* Check if the answer was correct
