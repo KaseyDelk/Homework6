96d57ce6eff3a5a982a9231c07c784e2d4ed2219
Kasey Delk <delkk@student.swosu.edu>
2015-10-14 09:31:19 -0500

    finished writing code with Dr. Evert

--- src/Homework6.java
+++ src/Homework6.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 public class Homework6 {
 	public static void main(String[] args) {
 
-				System.out.println("Hello Class!");
 				// This is my branch I use for classwork
 		
 				int score = 0;
@@ -19,12 +18,13 @@ public class Homework6 {
 				//Ask the user to add these two numbers together
 				System.out.println
 				("What is " + number1 + " + " + number2 + "?");
+				System.out.println
+				("Please anser in integers only.");
 				
 				//Read their response
 				Scanner input = new Scanner(System.in);
 				int studentAnswer = input.nextInt();
 				
-				//Check if the answer was correct
 				//IF correct
 				if (studentAnswer == correctAnswer){
 				//Tell them it was correct
@@ -43,7 +43,140 @@ public class Homework6 {
 				//Tell them the correct answer
 				System.out.println("The correct answer was: " + correctAnswer);
 				//Do not give them points
+				score += 0;
 				//Make the next question easier
+				if (hardnessOfQuestion>10);{
+					hardnessOfQuestion /= 10;
+
+				// Round 2				
+				//Generate 2 random numbers
+				number1 = (int)(Math.random() * hardnessOfQuestion);	
+				number2 = (int)(Math.random() * hardnessOfQuestion);
+				
+				//Note the correct answer.
+				correctAnswer = number1 + number2;
+					
+				//Ask the user to add these two numbers together
+				System.out.println
+				("What is " + number1 + " + " + number2 + "?");
+				System.out.println
+				("Please anser in integers only.");
+				
+				//Read their response
+				studentAnswer = input.nextInt();
+				
+				//IF correct
+				if (studentAnswer == correctAnswer){
+				//Tell them it was correct
+				System.out.println("Answer was correct");
+				//Give them points
+				score += hardnessOfQuestion;
+				System.out.println("Score is: " + score);
+				//Make the next question harder
+				hardnessOfQuestion *= 10;	
+				System.out.println("hardness is: " + hardnessOfQuestion);
+				}
+				//IF not correct
+				else{
+				//Tell them it was wrong
+				System.out.println("Answer was not correct");
+				//Tell them the correct answer
+				System.out.println("The correct answer was: " + correctAnswer);
+				//Do not give them points
+				score += 0;
+				//Make the next question easier
+				if (hardnessOfQuestion>10);{
+				hardnessOfQuestion /= 10;
+						
+				//Round 3
+					
+				//Generate 2 random numbers
+				number1 = (int)(Math.random() * hardnessOfQuestion);	
+				number2 = (int)(Math.random() * hardnessOfQuestion);
+					
+				//Note the correct answer.
+				correctAnswer = number1 + number2;
+					
+				//Ask the user to add these two numbers together
+				System.out.println
+				("What is " + number1 + " + " + number2 + "?");
+				System.out.println
+				("Please anser in integers only.");
+				
+				//Read their response
+				studentAnswer = input.nextInt();
+					
+				//IF correct
+				if (studentAnswer == correctAnswer){
+				//Tell them it was correct
+				System.out.println("Answer was correct");
+				//Give them points
+				score += hardnessOfQuestion;
+				System.out.println("Score is: " + score);
+				//Make the next question harder
+				hardnessOfQuestion *= 10;	
+				System.out.println("hardness is: " + hardnessOfQuestion);
+				}
+				//IF not correct
+				else{
+				//Tell them it was wrong
+				System.out.println("Answer was not correct");
+				//Tell them the correct answer
+				System.out.println("The correct answer was: " + correctAnswer);
+				//Do not give them points
+				score += 0;
+				//Make the next question easier
+				if (hardnessOfQuestion>10);{
+				hardnessOfQuestion /= 10;
+			
+				//Round 4
+					
+				//Generate 2 random numbers
+				number1 = (int)(Math.random() * hardnessOfQuestion);	
+				number2 = (int)(Math.random() * hardnessOfQuestion);
+					
+				//Note the correct answer.
+				correctAnswer = number1 + number2;
+					
+				//Ask the user to add these two numbers together
+				System.out.println
+				("What is " + number1 + " + " + number2 + "?");
+				System.out.println
+				("Please anser in integers only.");
+				
+				//Read their response
+				studentAnswer = input.nextInt();
+					
+				//IF correct
+				if (studentAnswer == correctAnswer){
+				//Tell them it was correct
+				System.out.println("Answer was correct");
+				//Give them points
+				score += hardnessOfQuestion;
+				System.out.println("Score is: " + score);
+				//Make the next question harder
+				hardnessOfQuestion *= 10;	
+				System.out.println("hardness is: " + hardnessOfQuestion);
+				}
+				//IF not correct
+				else{
+				//Tell them it was wrong
+				System.out.println("Answer was not correct");
+				//Tell them the correct answer
+				System.out.println("The correct answer was: " + correctAnswer);
+				//Do not give them points
+				score += 0;
+				//Make the next question easier
+				if (hardnessOfQuestion>10);{
+				hardnessOfQuestion /= 10;
+				
+									}
+								}
+							}
+						}
+					}
+				}
+			}
 		}
 	}
 }
